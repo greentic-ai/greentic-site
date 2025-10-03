6029c5886852398b333dc95f4a78eab6f677d993
Maarten Ectors <maarten@ectors.com>
2025-10-03 14:58:09 +0100

    updated version

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-/*export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }) => ({
+  base: "/greentic-site/",
   server: {
     host: "::",
     port: 8080,
@@ -16,7 +17,3 @@ import { componentTagger } from "lovable-tagger";
     },
   },
 }));
-*/
-export default defineConfig({
-  base: '/greentic-site/',
-});
